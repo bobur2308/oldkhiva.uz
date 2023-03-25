9c6c443dd65d15c027748365840fd1b948637178
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2023-03-25 20:27:16 +0500

    Changes

--- bot.js
+++ bot.js
@@ -3,8 +3,7 @@ const token = '5925050931:AAEERjHcMOaNV3bIwQtGnsWBaQyHxWHMNy0';
 const maker = 1135938477;
 const admin = 917489514;
 const admin_2 = 273832648;
-const {inf} = require('./Controllers/Controller')
-
+const {inf} = require('../Controllers/Controller')
 
 ;(async()=>{
   let response = await fetch(`https://api.telegram.org/bot${token}/sendMessage`,
